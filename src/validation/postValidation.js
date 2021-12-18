945779cf4b1a5cd4e001c0c0846fb83e9db63b81
THGMMX <morais.marques@hotmail.com>
2021-12-17 21:10:51 -0300

    updated

--- src/validation/postValidation.js
+++ src/validation/postValidation.js
@@ -5,16 +5,9 @@ import InvalidBodyRequestException from '../exceptions/InvalidBodyRequestExcepti
 
 const postValidation = async (body) => {
   const schema = yup.object().shape({
-    title: yup
-    .string()
-    .required('Required field')
-    .min(1, 'Mimimum of one charracter')
-    .max(150, 'Maximum of 150 charracters'),
-    description: yup
-    .string()
-    .required('Required field')
-    .min(10, 'Mimimum of 10 charracter')
-    .max(100, 'Maximum of 100 charracters'),
+    title: yup.string().required('Required field').min(1, 'Mimimum of one charracter').max(150, 'Maximum of 150 charracters'),
+    text: yup.string().required('Required field'),
+    description: yup.string().min(10, 'Mimimum of 10 charracter').max(100, 'Maximum of 100 charracters'),
     imageURL: yup.string().required('Required field'),
     });
     try {
